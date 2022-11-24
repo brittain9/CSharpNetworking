478ebba4855e7a79e0bef74c874671ac2d678815
brittain9 <alexbrittain9@gmail.com>
2022-11-24 05:58:35 -0600

    receiver

---
+++ UDPFileReceiver/NetworkMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPFileReceiver
+{
+    // This is a Simple data structure that is used in packet queues
+    class NetworkMessage
+    {
+        public IPEndPoint Sender { get; set; }
+        public Packet Packet { get; set; }
+    }
+}
